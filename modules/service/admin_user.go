cb4aa1da871a2f794ac8b33a8218d363adbf250c
阿炸克斯 <975794403@qq.com>
2023-09-15 10:23:15 +0800

    feat: jwt 方法封装

---
+++ modules/service/admin_user.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"server/dal/dao"
+	"server/dal/model"
+)
+
+type UserService struct {
+}
+
+func NewUserService() *UserService {
+	return new(UserService)
+}
+
+func (s *UserService) FindList() ([]*model.User, error) {
+	return dao.User.Find()
+}
+
+type CreateUser struct {
+	Name   string
+	Age    uint32
+	Email  string
+	Avatar string
+}
+
+func (s *UserService) FindByID(id uint) (*model.User, error) {
+	return dao.User.Where(dao.User.ID.Eq(id)).First()
+}
+
+func (s *UserService) Create(u CreateUser) *model.User {
+	info := &model.User{
+		Name:   u.Name,
+		Age:    u.Age,
+		Email:  u.Email,
+		Avatar: u.Avatar,
+	}
+	dao.User.Create(info)
+	return info
+}
